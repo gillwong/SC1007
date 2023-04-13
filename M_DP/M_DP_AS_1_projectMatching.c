6b52a39aaae3f9c54d687f271511b5d6d4328538
Gillbert Wong <66062290+gillwong@users.noreply.github.com>
2023-04-13 20:17:58 +0800

    updates projectMatching (broken)

--- M_DP/M_DP_AS_1_projectMatching.c
+++ M_DP/M_DP_AS_1_projectMatching.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef enum { SOURCE, PROJECT, STUDENT, MENTOR, SINK } SET;
+
 typedef struct _listnode
 {
     int vertex;
@@ -8,11 +10,19 @@ typedef struct _listnode
 } ListNode;
 typedef ListNode StackNode;
 
+typedef struct _graphnode
+{
+    SET to;
+    int vertex;
+} GraphNode;
 typedef struct _graph
 {
     int V;
     int E;
     ListNode **list;
+    // int project_cnt;
+    // int student_cnt;
+    // int mentor_cnt;
 } Graph;
 
 typedef ListNode QueueNode;
@@ -32,7 +42,10 @@ typedef struct _stack
 
 void insertAdjVertex(ListNode **AdjList, int vertex);
 void removeAdjVertex(ListNode **AdjList, int vertex);
-int matching(Graph g);
+// int matching(Graph g);
+int matching(int s_sz, ListNode **s, int t_sz, ListNode **t);
+
+int dfs(int s_sz, ListNode **s, int t_sz, ListNode **t, int start);
 
 void enqueue(Queue *qPtr, int item);
 int dequeue(Queue *qPtr);
@@ -57,21 +70,128 @@ int main()
     int np, nm; // number of projects and number of mentors
 
     // build graph
-    Graph g;
+    // Graph g;
     // Write your code
+    ListNode **source = malloc(Prj * sizeof(ListNode *));
+    for (int i = 0; i < Prj; ++i) source[i] = NULL;
+
+    ListNode **student = malloc(Std * sizeof(ListNode *));
+    for (int student_id = 0; student_id < Std; ++student_id) {
+        int student_num = student_id + 1;
+        student[student_id] = NULL;
+        
+        scanf("%d %d", &np, &nm);
+
+        for (int j = 0; j < np; ++j) {
+            int proj_num;
+            scanf("%d", &proj_num);
+            int proj_id = proj_num - 1;
+
+            ListNode *new = malloc(sizeof(ListNode));
+            new->vertex = student_num;
+            new->next = source[proj_id];
+            source[proj_id] = new;
+        }
+
+        for (int j = 0; j < nm; ++j) {
+            int mentor_num;
+            scanf("%d", &mentor_num);
+            int mentor_id = mentor_num - 1;
+
+            ListNode *new = malloc(sizeof(ListNode));
+            new->vertex = mentor_num;
+            new->next = student[student_id];
+            student[student_id] = new;
+        }
+    }
+
 
     // apply Ford Fulkerson algorithm
     //  use DFS or BFS to find a path
-    maxMatch = matching(g);
+    maxMatch = matching(Prj, source, Std, student);
     printf("%d\n", maxMatch);
     return 0;
 }
 
-int matching(Graph g)
+// int matching(Graph g)
+int matching(int s_sz, ListNode **s, int t_sz, ListNode **t)
 {
-
     // Write your code
-    return 0;
+    // copy network to residual network
+    ListNode **sf = malloc(s_sz * sizeof(ListNode *));
+    for (int i = 0; i < s_sz; ++i) {
+        sf[i] = NULL;
+
+        if (s[i] != NULL) {
+            ListNode *ptr = s[i];
+            ListNode *head = malloc(sizeof(ListNode));
+            head->vertex = ptr->vertex;
+            head->next = NULL;
+            ListNode *ptr_new = head;
+            ptr = ptr->next;
+
+            while (ptr) {
+                ListNode *new = malloc(sizeof(ListNode));
+                new->vertex = ptr->vertex;
+                new->next = NULL;
+                ptr_new->next = new;
+
+                ptr_new = ptr_new->next;
+                ptr = ptr->next;
+            }
+            sf[i] = head;
+        }
+    }
+
+    ListNode **tf = malloc(t_sz * sizeof(ListNode *));
+    for (int i = 0; i < t_sz; ++i) {
+        tf[i] = NULL;
+
+        if (t[i] != NULL) {
+            ListNode *ptr = t[i];
+            ListNode *head = malloc(sizeof(ListNode));
+            head->vertex = ptr->vertex;
+            head->next = NULL;
+            ListNode *ptr_new = head;
+            ptr = ptr->next;
+
+            while (ptr) {
+                ListNode *new = malloc(sizeof(ListNode));
+                new->vertex = ptr->vertex;
+                new->next = NULL;
+                ptr_new->next = new;
+
+                ptr_new = ptr_new->next;
+                ptr = ptr->next;
+            }
+            tf[i] = head;
+        }
+    }
+
+    int max_match = 0;
+    for (int i = 0; i < s_sz; ++i) {
+        if (dfs(s_sz, sf, t_sz, tf, i) != 0) ++max_match;
+    }
+    return max_match;
+}
+
+int dfs(int s_sz, ListNode **s, int t_sz, ListNode **t, int start)
+{
+    if (start < 0) return 0;
+    ListNode *ptr = s[start];
+    if (ptr == NULL) return 0;
+
+    while (ptr && dfs(s_sz, s, t_sz, t, ptr->vertex - 1) == 0) {
+        ptr = ptr->next;
+    }
+
+    if (ptr != NULL) {
+        ptr->vertex = -ptr->vertex;
+    } else {
+        return 0;
+    }
+
+    return 1;
 }
 
 void removeAdjVertex(ListNode **AdjList, int vertex)
