a45930f81409618da84476f88784337543b57038
Gillbert Wong <66062290+gillwong@users.noreply.github.com>
2023-04-14 15:57:54 +0800

    finishes projectMatching

--- M_DP/M_DP_AS_1_projectMatching.c
+++ M_DP/M_DP_AS_1_projectMatching.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct
+typedef struct _graph
 {
     int V;
     int prjCnt;
@@ -11,7 +11,7 @@ typedef struct
 } Graph;
 
 int matching(Graph *g);
-int dfs(Graph *const g, Graph *gf, char* visited, int start);
+int dfs(Graph *const gfPrev, Graph *gf, char* visited, int start, int dest);
 void graphcpy(Graph *src, Graph *dest);
 
 int main()
@@ -23,7 +23,7 @@ int main()
     int np, nm;
 
     Graph g;
-    g.V = Prj + Std + Mtr + 2;  // 2: source node & sink node
+    g.V = 1 + Prj + (2 * Std) + Mtr + 1;  // source node & sink node
     g.prjCnt = Prj;
     g.stdCnt = Std;
     g.mtrCnt = Mtr;
@@ -38,6 +38,12 @@ int main()
     for (int i = 1; i <= Prj; ++i) {
         g.adj[0][i] = 1;
     }
+    for (int i = 1 + Prj + 2 * Std; i < g.V - 1; ++i) {
+        g.adj[i][g.V - 1] = 1;
+    }
+    for (int i = 1 + Prj, j = 1 + Prj + Std; i < 1 + Prj + Std; ++i, ++j) {
+        g.adj[i][j] = 1;
+    }
 
     for (int i = 0; i < Std; ++i) {
         scanf("%d %d", &np, &nm);
@@ -49,12 +55,8 @@ int main()
         for (int j = 0; j < nm; ++j) {
             int mtr_num;
             scanf("%d", &mtr_num);
-            g.adj[1 + Prj + i][1 + Prj + Std + (mtr_num - 1)] = 1;
+            g.adj[1 + Prj + Std + i][1 + Prj + 2 * Std + (mtr_num - 1)] = 1;
         }
-    }
-
-    for (int i = 1 + Prj + Std; i < g.V - 1; ++i) {
-        g.adj[i][g.V - 1] = 1;
     }
 
     maxMatch = matching(&g);
@@ -64,34 +66,39 @@ int main()
 
 int matching(Graph *g)
 {
-    Graph *gf = malloc(sizeof(Graph));  // residual graph
+    // residual graph
+    Graph *gfPrev = malloc(sizeof(Graph)),
+          *gf = malloc(sizeof(Graph));
     graphcpy(g, gf);
-    char *visited = malloc(g->V * sizeof(char));
 
+    char *visited = malloc(gf->V * sizeof(char));
     do {
-        graphcpy(gf, g);
-        for (int i = 0; i < g->V; ++i) visited[i] = 0;
-    } while (dfs(g, gf, visited, 0));
+        graphcpy(gf, gfPrev);
+        for (int i = 0; i < gf->V; ++i) {
+            visited[i] = 0;
+        }
+    } while (dfs(gfPrev, gf, visited, 0, g->V - 1));
 
     int matches = 0;
-    for (int i = 1; i <= g->prjCnt; ++i) {
-        if (g->adj[i][0]) ++matches;
+    for (int i = 1; i <= gf->prjCnt; ++i) {
+        if (gf->adj[i][0] == 1) {
+            ++matches;
+        }
     }
 
-    free(visited);
-    free(gf);
+    free(visited); free(gf); free(gfPrev);
     return matches;
 }
 
-int dfs(Graph *const g, Graph *gf, char* visited, int start)
+int dfs(Graph *const gfPrev, Graph *gf, char* visited, int start, int dest)
 {
-    if (start == g->V - 1) return 1;
-    if (visited[start]) return 0;
+    if (start == dest) return 1;
+    if (visited[start] == 1) return 0;
     visited[start] = 1;
 
-    for (int i = 0; i < g->V; ++i) {
-        if (g->adj[start][i] == 1) {
-            int res = dfs(g, gf, visited, i);
+    for (int i = 0; i < gfPrev->V; ++i) {
+        if (gfPrev->adj[start][i] == 1) {
+            int res = dfs(gfPrev, gf, visited, i, dest);
             if (res) {
                 gf->adj[start][i] = 0;
                 gf->adj[i][start] = 1;
